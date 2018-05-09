5b845aa14e585a139a15d44dac14b877f9f702a8
Евгений Столяров <emirick@yandex.ru>
2018-05-09 21:24:07 +0300

    Added vacancy detail & rm

--- task_1/app/controllers/vacancies_controller.py
+++ task_1/app/controllers/vacancies_controller.py
@@ -7,3 +7,14 @@ def get_vacancies_list(page=None, page_length=None):
         result = result.offset((page - 1) * page_length).limit(page_length)
 
     return list(result)
+
+
+def get_vacancy(idx):
+    result = Vacancies.query.filter_by(id=idx).first()
+    return result
+
+
+def delete_vacancy(idx):
+    Vacancies.query.filter_by(id=idx).delete()
+    return None
+
--- task_1/app/views/vacancies_view.py
+++ task_1/app/views/vacancies_view.py
@@ -5,7 +5,7 @@ import ujson as ujson
 from flask import Blueprint, render_template, request, session, redirect, url_for
 
 from task_1.app import app, db_session
-from task_1.app.controllers.vacancies_controller import get_vacancies_list
+from task_1.app.controllers.vacancies_controller import get_vacancies_list, get_vacancy, delete_vacancy
 from task_1.app.models import Vacancies
 
 vacancies_view = Blueprint('vacancies_view', __name__, static_folder='static', template_folder='templates')
@@ -14,14 +14,15 @@ vacancies_view = Blueprint('vacancies_view', __name__, static_folder='static', t
 class RequiredException(Exception):
     pass
 
+
 @app.route('/', methods=['GET'])
 def index():
     return redirect(url_for('vacancies'))
 
 
 @app.route('/vacancies', methods=['GET', 'PUT'])
-def vacancies():
-    if request.method == 'PUT':
+def vacancies_page():
+    if request.method.lower() == 'put':
         try:
             data = {}
             for key, value in request.form.items():
@@ -38,16 +39,16 @@ def vacancies():
                 raise RequiredException('All columns must contain non-empty value')
 
             new_vacancy = Vacancies(**data)
-            db_session.commit()
-            return json.dumps({'success': True})
         except ValueError:
             db_session.rollback()
-            return json.dumps({'error': True, 'type': 'salary'})
+            return json.dumps({'error': 'salary'})
         except Exception as e:
             print(str(e))
             db_session.rollback()
             return json.dumps({'error': True})
-
+        else:
+            db_session.commit()
+            return json.dumps({'success': True})
 
     vacancies_list = get_vacancies_list()
     content = {'vacancies_list': vacancies_list}
@@ -55,6 +56,22 @@ def vacancies():
 
 
 @app.route('/vacancies/<idx>', methods=['GET', 'DELETE'])
-def vacancy(idx):
+def vacancy_page(idx):
+    if request.method.lower() == 'delete':
+        request_idx = request.form.get('id')
+        request_idx = request_idx[0] if isinstance(request, list) and len(request_idx) > 0 else request_idx
+        if request_idx != idx:
+            return json.dumps({'error': 'idx_mismatch'})
+        try:
+            delete_vacancy(idx)
+        except Exception as e:
+            print(str(e))
+            db_session.rollback()
+            return json.dumps({'error': True})
+        else:
+            db_session.commit()
+            return json.dumps({'success': url_for('vacancies_page')})
 
-    return render_template('vacancies.html', **content)
+    vacancy = get_vacancy(idx)
+    content = {'vacancy': vacancy, 'vacancy_name': str(vacancy)}
+    return render_template('vacancy.html', **content)
